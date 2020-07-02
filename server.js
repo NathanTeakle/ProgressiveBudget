4acdf250bc85f0fe756837c216f4c759818391b1
Nathan Teakle <n.teakle5071@gmail.com>
2020-07-02 19:10:41 +0930

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:password1@ds033757.mlab.com:33757/heroku_g8tlwjdz";
+mongoose.connect(MONGODB_URI);
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
